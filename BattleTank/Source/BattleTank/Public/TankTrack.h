bb0a0caa916bfd2c8bfcd5a428d35f13be04ae3c
Xun94 <siazhengxun@gmail.com>
2020-06-17 14:58:06 +0800

    Sideways Friction

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -20,4 +20,7 @@ public:
 
 	UPROPERTY(EditDefaultsOnly)
 	float TrackMaxDrivingForce = 40000000;
+private:
+	UTankTrack();
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
 };
